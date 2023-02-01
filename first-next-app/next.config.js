c20a7c53c2d841189aef51a071e39b05e6eeb1d3
MJ <80272444+minai621@users.noreply.github.com>
2023-02-01 06:01:05 +0000

    webpack custom

--- first-next-app/next.config.js
+++ first-next-app/next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.js/,
+      use: [
+        options.defaultLoaders.babel,
+        // example
+        // {
+        //   loader: 'my-custom-loader',
+        //   options: loaderOptions,
+        // },
+      ],
+    });
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
