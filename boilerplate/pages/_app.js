3d1e1de6455dd10c1e8581d4851782404f5140f4
MJ <80272444+minai621@users.noreply.github.com>
2023-02-01 10:48:45 +0000

    shopping Card Provider

--- boilerplate/pages/_app.js
+++ boilerplate/pages/_app.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import ShoppingCartContext from '../components/context/cartContext';
 
 function MyApp({ Component, pageProps }) {
+  const [items, setItems] = useState({});
   return (
     <>
       <Head>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+        <link
+          href='https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css'
+          rel='stylesheet'
+        />
       </Head>
       <Navbar />
-      <div className="w-9/12 m-auto pt-10">
-        <Component {...pageProps} />
-      </div>
+      <ShoppingCartContext.Provider value={{ items, setItems }}>
+        <div className='w-9/12 m-auto pt-10'>
+          <Component {...pageProps} />
+        </div>
+      </ShoppingCartContext.Provider>
     </>
   );
 }
