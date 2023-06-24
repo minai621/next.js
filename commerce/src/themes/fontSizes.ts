b1a3f3c9a3f7dc78dcf20c0c69c7622a448fad90
MJ <80272444+minai621@users.noreply.github.com>
2023-06-24 07:57:29 +0000

    create themes

---
+++ commerce/src/themes/fontSizes.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const fontSizes: any = [12, 14, 16, 20, 24, 32]
+
+fontSizes.extraSmall = fontSizes[0]
+fontSizes.small = fontSizes[1]
+fontSizes.medium = fontSizes[2]
+fontSizes.mediumLarge = fontSizes[3]
+fontSizes.extraLarge = fontSizes[4]
+
+export default fontSizes
